152844b6759c77cdc11a088b354a9d76bfac137d
Akilaxn <85438544+Akilan-droid@users.noreply.github.com>
2021-12-02 21:29:38 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,16 +1,38 @@
-#include <avr/io.h>
-#include"A1.h"
-#include"A2.h"
-#include"A3.h"
-#include<util/delay.h>
+  
+/**
+ * @file main.c
+ * @author Akilan B
+ * @brief Heat Seating Application to set Temperature of seat
+ * @version 0.1
+ * @date 2021-02-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+
 int main(void)
 {
-    LCD_INITIALIZATION();
+    uint16_t tmp;
+    
     while(1)
     {
-        A1(); //Activity 1
-        A2(); //Activity 2
-        A3(); //Activity 3
+        if(LED()==1) //Check if both the switches are pressed
+        {
+            LED_ON();          //Turn LED ON
+            tmp=GetADC();     //To Get the ADC value
+            PWM(tmp);         //this is to PWM output based on temperature
+		        USARTWrite(tmp); //this is for Serial monitor to display Temperature
+        }
+        else  // other cases
+        {
+            LED_OFF();     //Turn LED OFF
+	    _delay_ms(200);
+        }
     }
-  return 0;
+    return 0;
 }
